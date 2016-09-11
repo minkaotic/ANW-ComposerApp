e2bbd739ed1a6872323d914cb0cfd6ec2f96c6db
minkaotic <minkphole@gmail.com>
2016-09-11 10:22:36 +0100

    Set up test project, using Coypu and Nunit

---
+++ ANW.ComposerApp_Tests/Index_page.cs
@@ -0,0 +1,35 @@
+﻿using Coypu;
+using Coypu.NUnit.Matchers;
+using NUnit.Framework;
+
+namespace ANW.ComposerApp_Tests
+{
+    [TestFixture]
+    public class Index_page
+    {
+        private BrowserSession _browser;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _browser = new BrowserSession(new SessionConfiguration
+            {
+                AppHost = "localhost",
+                Port = 55488
+            });
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            _browser.Dispose();
+        }
+
+        [Test]
+        public void Contains_Audionetwork_header()
+        {
+            _browser.Visit("/");
+            Assert.That(_browser, Shows.Content("Audionetwork"));
+        }
+    }
+}
